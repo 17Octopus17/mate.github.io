12aac12952e65de91e839ec0b74abe41698800c9
17Octopus17 <kuznetsova.olga17@gmail.com>
2019-02-21 19:18:53 +0200

    Updates

---
+++ precache-manifest.c197567a874d8986507d498bb823db6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "efdcb16ff3b458cce346",
+    "url": "/static/css/main.5bf7d417.chunk.css"
+  },
+  {
+    "revision": "efdcb16ff3b458cce346",
+    "url": "/static/js/main.efdcb16f.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "838835256c241f9b6b64",
+    "url": "/static/js/2.83883525.chunk.js"
+  },
+  {
+    "revision": "710abe0dc42e677ac8b337668a499b6d",
+    "url": "/index.html"
+  }
+];
